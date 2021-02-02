fd55502e1ae7c02dc454409ef6b121db79244f72
priyanka <priyanka.samantaroy@gmail.com>
2021-02-02 19:56:08 +0000

    feting data using getAll to get the books data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+    books: []
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+   // showSearchPage: false
+  }
+  //using BooksAPI getALL method that fetch books data
+  componentDidMount(){
+    BooksAPI.getAll()
+    .then((books)=>{
+      this.setState(()=> ({books}))
+    })
   }
 
   render() {
+    console.log('books array',this.state.books)
     return (
       <div className="app">
         {this.state.showSearchPage ? (
