75b167eb3afa9511712add43abc53a7a1581074e
priyanka <priyanka.samantaroy@gmail.com>
2021-03-01 18:41:42 +0000

    style: added transform and transition property
    Added css style transform in list component
    This property will enlarge the list view while hover on
    
    Also added css style transition property that will make the transform with times added to transition.

--- src/Rack.js
+++ src/Rack.js
@@ -22,7 +22,7 @@ export class Rack extends Component {
                         <li key={book.id}>
                           <div className="book">
                             <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail})` }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193,backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
                             <div className="book-shelf-changer"> 
                             <select id={book.id} onChange={(e)=>shelfToShelf(e)}>
                             <option value="moveTo" selected> Move to...</option>                       
